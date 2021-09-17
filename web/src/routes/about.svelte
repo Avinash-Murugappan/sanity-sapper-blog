af93e7dc2d22ba2b50af9c6a8f17c67900f1163c
Avi <avinash.arunachalachettiarmurugappan@legrand.com>
2021-09-17 16:52:04 -0500

    UI/UX Updates

--- web/src/routes/about.svelte
+++ web/src/routes/about.svelte
@@ -1,7 +1,33 @@
+<style>
+  figure {
+    float: right;
+    margin: -3em -7em 2em 2em;
+    max-width: 400px;
+  }
+  figure img {
+    border-radius: 8px;
+  }
+  figcaption{
+    font-weight: bold;
+    text-align: center;
+  }
+  @media (max-width: 1020px) {
+    figure {
+      float: none;
+      margin: 0 auto 2em;
+    }
+  }
+</style>
+
 <svelte:head>
-	<title>About</title>
+  <title>Avi's Blog | About</title>
 </svelte:head>
 
-<h1>About this site</h1>
-
-<p>This is the 'about' page. There's not much here.</p>
+<div class="container">
+  <h1>About</h1>
+  <figure>
+    <img src='avi.jpg' alt="Avi's cool Picture.">
+    <figcaption>Go Chiefs!!!</figcaption>
+  </figure>
+  <p>I’m Avinash, who enjoys drinking coffee and developing software, whether that be websites, applications, or anything in between.</p>
+</div>
--- web/src/routes/blog/index.svelte
+++ web/src/routes/blog/index.svelte
@@ -19,24 +19,56 @@
 </script>
 
 <style>
-	ul {
+    h2,
+  .post-item-footer {
+    font-family: Rubik, sans-serif;
+    font-weight: 700;
+  }
+  .post-item-date {
+    color: #AAA;
+    text-align: left;
+    text-transform: uppercase;
+    margin-right: 16px;
+  }
+  hr {
+    margin: 60px auto;
+  }
+	/* ul {
 		margin: 0 0 1em 0;
 		line-height: 1.5;
-	}
+	} */
 </style>
 
 <svelte:head>
-	<title>Blog</title>
+	<title>Avi's Blog | Blog</title>
 </svelte:head>
 
-<h1>Recent posts</h1>
+<!-- <h1>Recent posts</h1>
 
 <ul>
-	{#each posts as post}
+	{#each posts as post} -->
 		<!-- we're using the non-standard `rel=prefetch` attribute to
 				tell Sapper to load the data for the page as soon as
 				the user hovers over the link or taps it, instead of
 				waiting for the 'click' event -->
-		<li><a rel='prefetch' href='blog/{post.slug.current}'>{post.title}</a> ({formatDate(post.publishedAt)})</li>
+		<!-- <li><a rel='prefetch' href='blog/{post.slug.current}'>{post.title}</a> ({formatDate(post.publishedAt)})</li>
 	{/each}
-</ul>
+</ul> -->
+
+<div class="container">
+  <h1>Blog</h1>
+  {#each posts as post, index}
+    {#if index}
+      <hr />
+    {/if}
+    <div class="post-item">
+      <h2>
+        <a rel='prefetch' href='blog/{post.slug.current}'>{post.title}</a>
+      </h2>
+      <p rel='prefetch' >{post.excerpt[0].children[0].text}</p>
+      <div class="post-item-footer">
+        <span class="post-item-date">({formatDate(post.publishedAt)})</span>
+      </div>
+    </div>
+  {/each}
+</div>
